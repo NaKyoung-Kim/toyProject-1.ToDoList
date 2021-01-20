7e46d2406dee9d5c14a31aa4b63a747cd5421970
a419802 <a419802@users.noreply.github.com>
2021-01-20 21:36:24 +0900

    Add "weather.js"
    브라우저로부터 사용자의 위치를 읽어온 후, 사용자가 있는 곳의 마을 이름과 현재 날씨를 띄워준다.

---
+++ js/weather.js
@@ -0,0 +1,56 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "3986b7662f095bcebad8e690a9c54314";
+const COORDS = 'coords';
+
+function getWeather(lat, lon){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&&units=metric`)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj){
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function getGeoSuccess(position){
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  }
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function getGeoError(){
+  console.log(`Can't access geo location`);
+}
+
+function getCoords(){
+  navigator.geolocation.getCurrentPosition(getGeoSuccess, getGeoError);
+}
+
+function loadCoords(){
+  const loadedCoords = localStorage.getItem(COORDS);
+  
+  if(loadedCoords === null){
+    getCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init(){
+  loadCoords();
+};
+
+init();
